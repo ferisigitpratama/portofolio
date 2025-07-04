4640765d855346b8c1b22f17307ca68411c390ca
ferisigitpratama <ferisigitpratama@gmail.com>
2025-07-04 01:11:04 +0000

    a

--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,8 @@ return (
   <div className="min-h-screen overflow-x-hidden">
     <div className="container border mx-auto h-screen">
       <div className="grid grid-cols-12">
-        <div className="cool-span-6">A</div>
-        <div className="cool-span-6">B</div>
+        <div className="col-span-6">A</div>
+        <div className="col-span-6">B</div>
       </div>
     </div>
   </div>
